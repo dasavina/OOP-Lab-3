70f3637b3700f426c18881a837753ccbcb947fd7
Savina Daria <savina.dariia@chnu.edu.ua>
2023-09-17 18:39:45 +0300

    task 1 done

--- 1. Define a Class Person/1. Define a Class Person.cs
+++ 1. Define a Class Person/1. Define a Class Person.cs
@@ -1,1 +1,38 @@
-﻿
+﻿public class Person
+{
+    string name = "Gosho";
+    int age = 18;
+
+    public int Age
+    {
+        set { age = value; }
+        get { return age; }
+    }
+    public string Name
+    {
+        set { name = value; }
+        get { return name; }
+        }
+
+
+    }
+
+class Program
+{
+    private static void Main()
+    {
+        Person pesho = new Person();
+        pesho.Age = 20;
+        pesho.Name = "Pesho";
+
+        Person gosho = new Person();
+       
+        Person stamat = new Person();
+        stamat.Age = 43;
+        stamat.Name = "Stamat";
+
+        Console.WriteLine("name: {0}; age: {1}", pesho.Name, pesho.Age) ;
+        Console.WriteLine("name: {0}; age: {1}", gosho.Name, gosho.Age) ;
+        Console.WriteLine("name: {0}; age: {1}", stamat.Name, stamat.Age) ;
+    }
+}
