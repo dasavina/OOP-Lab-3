60330b5f4019cc1ab9ad1f94bc298dc11ff3e43a
Savina Daria <savina.dariia@chnu.edu.ua>
2023-09-17 19:57:15 +0300

    task 2; started task 3

--- 1. Define a Class Person/1. Define a Class Person.cs
+++ 1. Define a Class Person/1. Define a Class Person.cs
@@ -12,10 +12,25 @@
     {
         set { name = value; }
         get { return name; }
-        }
-
-
     }
+
+    public Person()
+    {
+        Age = 1;
+        Name = "No name";
+    }
+
+    public Person(int age)
+    { this.Age = age; Name = "No name"; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+
 
 class Program
 {
@@ -26,13 +41,13 @@ class Program
         pesho.Name = "Pesho";
 
         Person gosho = new Person();
-       
+
         Person stamat = new Person();
         stamat.Age = 43;
         stamat.Name = "Stamat";
 
-        Console.WriteLine("name: {0}; age: {1}", pesho.Name, pesho.Age) ;
-        Console.WriteLine("name: {0}; age: {1}", gosho.Name, gosho.Age) ;
-        Console.WriteLine("name: {0}; age: {1}", stamat.Name, stamat.Age) ;
+        Console.WriteLine("name: {0}; age: {1}", pesho.Name, pesho.Age);
+        Console.WriteLine("name: {0}; age: {1}", gosho.Name, gosho.Age);
+        Console.WriteLine("name: {0}; age: {1}", stamat.Name, stamat.Age);
     }
 }
---
+++ 1. Define a Class Person/3. Family.cs
@@ -0,0 +1,33 @@
+﻿class Family
+{
+    List<Person> members = new List<Person>();
+
+    void AddMember(Person member)
+    {
+        members.Add(member);
+    }
+    Person GetOldestMember()
+    {
+        Person oldest = new Person(0);
+        foreach (Person member in members)
+        {
+            if (member.Age > oldest.Age)
+            { oldest = member; }
+        }
+        return oldest;
+    }
+
+}
+
+class Task2
+{
+    static public void Main()
+    {
+        Console.WriteLine("quantity of people:");
+
+        int quantity = Console.Read();
+
+        Console.WriteLine("input the names and ages of family members")
+
+    }
+}
