67dc2dc8003a995bf25e7e7411e82db696a27bd8
Savina Daria <savina.dariia@chnu.edu.ua>
2023-09-17 20:22:29 +0300

    task 3 finished

--- 1. Define a Class Person/3. Family.cs
+++ 1. Define a Class Person/3. Family.cs
@@ -1,12 +1,14 @@
 ﻿class Family
 {
     List<Person> members = new List<Person>();
+    List<Person> Members
+    { get; set; }
 
-    void AddMember(Person member)
+    public void AddMember(Person member)
     {
         members.Add(member);
     }
-    Person GetOldestMember()
+    public Person GetOldestMember()
     {
         Person oldest = new Person(0);
         foreach (Person member in members)
@@ -26,8 +28,19 @@ class Task2
         Console.WriteLine("quantity of people:");
 
         int quantity = Console.Read();
+        Family Members = new Family();
 
-        Console.WriteLine("input the names and ages of family members")
+        Console.WriteLine("input the names and ages of family members");
+        for (int i = 0; i < quantity; i++)
+        {
+            Console.WriteLine("name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("age:");
+            int age = Console.Read();
+            Person member = new Person(name, age);
+            Members.AddMember(member);
+        }
 
+        Console.WriteLine(Members.GetOldestMember());
     }
 }
